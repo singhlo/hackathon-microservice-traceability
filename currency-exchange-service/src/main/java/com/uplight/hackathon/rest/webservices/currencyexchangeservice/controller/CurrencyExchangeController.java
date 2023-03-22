3d5b40c8231b41fbd91a874113cd1e7ef75d161c
“ff-sdesai” <sumit.desai@uplight.com>
2023-03-22 16:52:11 +0530

    Merge branch 'main' of github.com:singhlo/hackathon-microservice-traceability
    Added dependencies

--- currency-exchange-service/src/main/java/com/uplight/hackathon/rest/webservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/uplight/hackathon/rest/webservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,48 +1,49 @@
 package com.uplight.hackathon.rest.webservices.currencyexchangeservice.controller;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uplight.hackathon.rest.webservices.currencyexchangeservice.bean.CurrencyExchange;
 import com.uplight.hackathon.rest.webservices.currencyexchangeservice.repository.controller.CurrencyExchangeRepository;
 
 /**
- * 
  * @author Lokendra Singh
  * @date 19 March 2023
- * 
- * The controller class to expose currency-exchange micro service APIs.
- *
+ *       The controller class to expose currency-exchange micro service APIs.
  */
 @RestController
 public class CurrencyExchangeController {
-	@Autowired
-	private Environment environment;
-	@Autowired
-	private CurrencyExchangeRepository currencyExchangeRepository;
-	
-	/**
-	 * Retrives exchange rates from the database based on the type of currencies.
-	 * 
-	 * @param from
-	 * @param to
-	 * @return CurrencyExchange
-	 */
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
-	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		CurrencyExchange currencyExchange1 = currencyExchangeRepository.findByFromAndTo(from, to);
-		if(currencyExchange1 == null) {
-			throw new RuntimeException("Unable to find data for " + from + "And" + to);
-		}
-		CurrencyExchange currencyExchange=  new CurrencyExchange(1000L, from, to , BigDecimal.valueOf(65));
-		String port = environment.getProperty("local.server.port");
-		currencyExchange1.setEnvironment(port);
-		return currencyExchange1;
-	}
+    @Autowired
+    private Environment environment;
+    @Autowired
+    private CurrencyExchangeRepository currencyExchangeRepository;
+
+    /**
+     * Retrives exchange rates from the database based on the type of currencies.
+     * @param from
+     * @param to
+     * @return CurrencyExchange
+     */
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue( @PathVariable String from, @PathVariable String to, @RequestHeader Map<String, String> headers ) {
+        CurrencyExchange currencyExchange1 = currencyExchangeRepository.findByFromAndTo( from, to );
+
+        System.out.println( "headers in retrieveExchangeValue are " + headers );
+
+        if ( currencyExchange1 == null ) {
+            throw new RuntimeException( "Unable to find data for " + from + "And" + to );
+        }
+        CurrencyExchange currencyExchange = new CurrencyExchange( 1000L, from, to, BigDecimal.valueOf( 65 ) );
+        String port = environment.getProperty( "local.server.port" );
+        currencyExchange1.setEnvironment( port );
+        return currencyExchange1;
+    }
 
 }
